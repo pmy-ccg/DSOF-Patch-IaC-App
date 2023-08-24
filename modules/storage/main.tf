3688f2c6de7b5fa8d9b833d438d8ea0e420da6fd
pmy-ccg <96039170+pmy-ccg@users.noreply.github.com>
2023-08-24 14:56:17 +0800

    Secured S3 bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,24 +1,34 @@
 resource "aws_s3_bucket" "insecure-bucket" {
   bucket = "insecure-bucket"
-   versioning.enabled      = true
-   logging                 = true
-   versioning.mfa_delete   = true
 }
 
-resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
-   block_public_acls       = true
-   block_public_policy     = true
-   ignore_public_acls      = true
-   restrict_public_buckets = true
-   versioning.enabled      = true
-   logging                 = true
-   versioning.mfa_delete   = true
- }
+resource "aws_s3_bucket_public_access_block" "insecure-bucket-block" {
+  bucket = aws_s3_bucket.insecure-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_logging" "insecure-bucket-logging" {
+  bucket = aws_s3_bucket.insecure-bucket.id
+
+  target_bucket = "your-logging-bucket-name"
+}
+
+resource "aws_s3_bucket_versioning" "insecure-bucket-versioning" {
+  bucket = aws_s3_bucket.insecure-bucket.id
+
+  enabled = true
+  mfa_delete = true
+}
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 20
   encrypted         = true
+
   tags = {
     Name = "insecure"
   }
