9a85ca59284fe2034ac9a5c448fa5a5f7a5401b4
pmy-ccg <96039170+pmy-ccg@users.noreply.github.com>
2023-08-24 14:34:57 +0800

    Fixed insecure configurations

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -2,4 +2,8 @@ resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_logging = true
+  enable_log_file_validation = true
+  kms_key_id = "mock_kms_key"
+  is_multi_region_trail = true
+  cloud_watch_logs_group_arn = "mock_arn"
 }
